82fbd98c2a71e9e09068655dde1b7dd8f280ac64
Aurelius Dilectio Yuanata Djatmiko <aureliusdilectioyd@gmail.com>
2021-09-07 19:41:07 +0700

    File Project Pong

---
+++ Pong/Assets/Scripts/SideWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    public PlayerControl player;
+    [SerializeField]
+    private GameManager gameManager;
+    
+    void OnTriggerEnter2D(Collider2D otherCollider)    
+    {
+        if(otherCollider.name == "Ball")
+        {
+            player.IncrementScore();
+
+             if(player.Score < gameManager.maxScore)
+             {
+                 otherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+             }
+        }
+    }
+
+}//class
